859d0da420a0ffc8db968176f2765e5658356016
Geir Gåsodden <geir.gasodden@pythonia.no>
2016-02-15 10:55:32 +0100

    Removes unused config

--- config/index.js
+++ config/index.js
@@ -3,7 +3,6 @@
 var config = {
   JWT_KEY: process.env.JWT_KEY || 'NeverShareYourSecret',
   CALLBACK_STATUS_MESSAGE: process.env.CALLBACK_STATUS_MESSAGE || 'Varselbrev produsert',
-  CALLBACK_STATUS_URL: process.env.CALLBACK_STATUS_URL || false,
   JOB_DIRECTORY_PATH: process.env.JOB_DIRECTORY_PATH || 'test/data/jobs',
   DISTRIBUTION_DIRECTORY_PATH: process.env.DISTRIBUTION_DIRECTORY_PATH || 'test/data/distribution',
   ARCHIVE_DIRECTORY_PATH: process.env.ARCHIVE_DIRECTORY_PATH || 'test/data/archive',
