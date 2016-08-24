283e50bc9b29ddbd96cf7ee65ec5fd9d1597de0d
Geir Gåsodden <geir.gasodden@pythonia.no>
2016-08-24 10:48:19 +0200

    Adds missing dependency

--- config/index.js
+++ config/index.js
@@ -1,6 +1,6 @@
 'use strict'
 
-var config = {
+module.exports = {
   JWT_KEY: process.env.TFK_SEV_JWT_KEY || 'Louie Louie, oh no, I got to go Louie Louie, oh no, I got to go',
   CALLBACK_STATUS_MESSAGE: process.env.TFK_SEV_CALLBACK_STATUS_MESSAGE || 'Varselbrev produsert',
   JOB_DIRECTORY_PATH: process.env.TFK_SEV_JOB_DIRECTORY_PATH || 'test/data/jobs',
@@ -19,5 +19,3 @@ var config = {
   P360WS_USER: process.env.TFK_SEV_P360WS_USER || 'domain/username',
   P360WS_PASSWORD: process.env.TFK_SEV_P360WS_PASSWORD || 'password'
 }
-
-module.exports = config
