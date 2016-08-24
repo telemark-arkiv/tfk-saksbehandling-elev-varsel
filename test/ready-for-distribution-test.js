65950e97833265ad49e8636075ef0d3b51aa24fa
Geir Gåsodden <geir.gasodden@pythonia.no>
2016-08-24 08:07:52 +0200

    Adds title generation for temnplates. Adds method for distribution check

---
+++ test/ready-for-distribution-test.js
@@ -0,0 +1,12 @@
+'use strict'
+
+const tap = require('tap')
+const readyForDistribution = require('../lib/ready-for-distribution')
+const readyData = require('./data/ready-for-distribution.json')
+const notReadyData = require('./data/ready-for-distribution-not.json')
+
+tap.equal(readyForDistribution(readyData), true, 'Returns true if ready')
+
+notReadyData.forEach(item => {
+  tap.equal(readyForDistribution(item), false, 'Returns false if not ready')
+})
