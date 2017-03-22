e8453200ff00cd2789bf910099749b24e667bfcf
Geir Gåsodden <geir.gasodden@pythonia.no>
2017-03-22 13:04:12 +0100

    Adds check for SPES-KD !== 0

--- test/lib/ready-for-distribution-test.js
+++ test/lib/ready-for-distribution-test.js
@@ -5,7 +5,9 @@ const readyForDistribution = require('../../lib/ready-for-distribution')
 const readyData = require('./../data/ready-for-distribution.json')
 const notReadyData = require('./../data/ready-for-distribution-not.json')
 
-tap.equal(readyForDistribution(readyData), true, 'Returns true if ready')
+readyData.forEach(item => {
+  tap.equal(readyForDistribution(item), true, 'Returns true if ready')
+})
 
 notReadyData.forEach(item => {
   tap.equal(readyForDistribution(item), false, 'Returns false if not ready')
